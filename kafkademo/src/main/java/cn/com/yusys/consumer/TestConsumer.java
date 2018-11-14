56f26424da2860182ebc7a67386903d99032ef1c
jdzw2018 <949465194@qq.com>
2018-11-14 14:40:07 +0800

    update

--- kafkademo/src/main/java/cn/com/yusys/consumer/TestConsumer.java
+++ kafkademo/src/main/java/cn/com/yusys/consumer/TestConsumer.java
@@ -2,6 +2,7 @@ package cn.com.yusys.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,9 +10,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TestConsumer {
-
+    //一条消息会分配到topic下的某一个分区内，
     @KafkaListener(topics = "test")
-    public void listen (ConsumerRecord<?, ?> record) throws Exception {
-        System.out.printf("topic = %s, offset = %d, value = %s \n", record.topic(), record.offset(), record.value());
+    public void listen1 (ConsumerRecord<?, ?> record) throws Exception {
+        //System.out.printf("topic = %s, offset = %d, value = %s \n", record.topic(), record.offset(), record.value());
+        System.out.println("listen1: "+record.toString());
+    }
+    @KafkaListener(topics = "test")
+    public void listen2(ConsumerRecord<?,?> record){
+        System.out.println("listen2: "+record.toString());
+    }
+    @KafkaListener(topics = "test")
+    public void listen3(ConsumerRecord<?,?> record){
+        System.out.println("listen3："+record.toString());
     }
 }
